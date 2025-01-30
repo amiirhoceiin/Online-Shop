02d08acde08f9114c3790221c38ca0363d27398d
amiirhoceiin <amirkhodadady81@gmail.com>
2025-01-30 12:03:40 +0330

    change loginform

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginForm.css';
 import { NavLink } from 'react-router-dom';
 
 export default function LoginForm() {
+   const [username,setUsername] = useState('');
+   const [password,setPassword] = useState('');
+   const [errorMessage, setErrorMessage] = useState('');
+
+     
+    const handelsubmit = async(e)=>{
+        e.preventDefault();
+        console.log(username);
+        const data = {username,password};
+        try {
+            const response = await fetch('http://your-backend-url/auth', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+            const result = await response.json();
+            
+            if (response.ok && result.token) {
+                
+                localStorage.setItem('token', result.token);
+                alert('ورود/ثبت نام موفقیت‌آمیز!');
+               
+            } else {
+                
+                setErrorMessage(result.message || 'خطایی در ورود پیش آمده');
+            }
+        }catch (error) {
+            
+            setErrorMessage('خطای شبکه، لطفاً دوباره تلاش کنید');
+            console.error('Error:', error);
+        }
+    }
+
   return (
     <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
-           <form className="container containerstyle p-4">
+           <form className="container containerstyle p-4" onSubmit={(e)=>handelsubmit(e)}>
         <div className='formlogo mb-5'>
           Boook
         </div>
         <div className='formname mb-3'>ورود/ثبت نام</div>
         <div className=" formbodystyle">
           <label htmlFor="exampleInputPassword1" className="form-label mt-1">سلام!<br></br>لطفا نام کاربری و رمز عبور خود را وارد کنید</label>
-          <input className='form-control mt-3' type="tel" name="phone" required placeholder='نام کاربری'/>
-          <input className='form-control mt-3 mb-4' type="tel" name="phone" required placeholder='رمز عبور'/>
+          <input className='form-control mt-3' value={username} type="tel" name="phone" required placeholder='نام کاربری' onChange={(e)=>setUsername(e.target.value)}/>
+          
+          <input className='form-control mt-3 mb-4' value={password} type="tel" name="phone" required placeholder='رمز عبور'
+          onChange={(e)=>setPassword(e.target.value)}/>
           
         </div>
 
