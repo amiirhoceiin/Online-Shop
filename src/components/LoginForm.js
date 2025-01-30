78da560c55d25d38b4ddaf7ec7d6a7243f29416c
amiirhoceiin <amirkhodadady81@gmail.com>
2025-01-30 18:06:42 +0330

    change passinput

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,7 +5,12 @@ import { NavLink } from 'react-router-dom';
 export default function LoginForm() {
    const [username,setUsername] = useState('');
    const [password,setPassword] = useState('');
+   const [showPasswordBtn, setShowPasswordBtn] = useState(false);
    const [errorMessage, setErrorMessage] = useState('');
+
+   const handelPass = (e)=>{
+    setPassword(e.target.value)
+   }
 
      
     const handelsubmit = (e)=>{
@@ -40,17 +45,24 @@ export default function LoginForm() {
 
   return (
     <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
-           <form className="container containerstyle p-4" onSubmit={(e)=>handelsubmit(e)}>
+           <form className="container containerstyle p-4" onSubmit={handelsubmit}>
         <div className='formlogo mb-5'>
           Boook
         </div>
         <div className='formname mb-3'>ورود/ثبت نام</div>
         <div className=" formbodystyle">
-          <label htmlFor="exampleInputPassword1" className="form-label mt-1">سلام!<br></br>لطفا نام کاربری و رمز عبور خود را وارد کنید</label>
-          <input className='form-control mt-3' value={username} type="tel" name="phone" required placeholder='نام کاربری' onChange={(e)=>setUsername(e.target.value)}/>
+          <label  className="form-label mt-1">سلام!<br></br>لطفا نام کاربری و رمز عبور خود را وارد کنید</label>
+          <input className='form-control mt-3' value={username} type="text" name="phone" required placeholder='نام کاربری' onChange={(e)=>setUsername(e.target.value)}/>
           
-          <input className='form-control mt-3 mb-4' value={password} type="tel" name="phone" required placeholder='رمز عبور'
-          onChange={(e)=>setPassword(e.target.value)}/>
+          <input className='form-control mt-3 mb-1' value={password} type={showPasswordBtn ? "text" : "password"}  name="phone" required placeholder='رمز عبور' minLength={8} maxLength={20}
+            onCopy={(e) => e.preventDefault()} 
+            onPaste={(e) => e.preventDefault()}
+            pattern="^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$"
+            title="رمز عبور باید شامل حداقل ۸ کاراکتر، یک حرف بزرگ، یک حرف کوچک، یک عدد و یک کاراکتر خاص باشد"
+          onChange={handelPass}/>
+          
+          <input className='mb-5' type="checkbox" onClick={()=>setShowPasswordBtn(!showPasswordBtn)}/>
+          <label style={{fontSize:'small'}}>&nbsp;نمایش</label>
           
         </div>
 
