f8bcc03d045328dd376dc1f2413404c8e4290110
amiirhoceiin <amirkhodadady81@gmail.com>
2025-02-09 23:00:24 +0330

    update carousel component

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Carousel.css'
 import Slide from '../img/slide/db4d9578201897b7c050248c6a4bfbd2.png'
 import { useTheme } from '../hooks/useTheme';
+import useFetch from '../hooks/useFetch';
 
 export default function Carousel() {
   const {mode} = useTheme();
+  const {data:book,isLoading,error} = useFetch('http://localhost:3000/books');
+  const [sliderBooks,setSliderBooks] = useState([]);
+
+      useEffect(() => {
+          if (book) {
+            setSliderBooks(book.slice(0, 5));
+          }
+        }, [book]); 
+
+        if (isLoading) {
+          return <div>Loading...</div>;
+        }
+      
+        if (error) {
+          return <div>Error: {error.message}</div>;
+        }
+
+
   return (
     <div className='Slider'> 
         <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
@@ -15,25 +34,20 @@ export default function Carousel() {
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
   </div>
-  <div className="carousel-inner ">
-    <div className="carousel-item active">
-      <img src={Slide} className="d-block w-100" alt="..."/>
-    </div>
-    <div className="carousel-item">
-      <img src={Slide} className="d-block w-100" alt="..."/>
-    </div>
-    <div className="carousel-item">
-      <img src={Slide} className="d-block w-100" alt="..."/>
-    </div>
-    <div className="carousel-item">
-      <img src={Slide} className="d-block w-100" alt="..."/>
-    </div>
-    <div className="carousel-item"> 
-      <img src={Slide} className="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  <div className="custom-controls"> 
 
+  
+  <div className="carousel-inner ">
+  {sliderBooks.map((sliderBook,index)=>(
+      <div key={sliderBook.id} 
+      className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+        <img src={sliderBook.imageSlider} className="d-block w-100" alt="..."/>
+      </div>
+     ))}
+  </div>
+
+
+
+  <div className="custom-controls">
       <button className={`carousel-control-prev ${mode}`} type="button" data-bs-target="#carouselExampleIndicators"     
        data-bs-slide="prev">
        <i className="fa-solid fa-arrow-left"></i>
