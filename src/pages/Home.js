4f361cd5d71e3e93d37e4de43875fb7c613a147a
amiirhoceiin <amirkhodadady81@gmail.com>
2025-02-27 23:04:36 +0330

    home component change design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ import Footer from '../components/Footer'
 
 export default function Home() {
   return (
-    <div  style={{width:'100%',maxWidth:'1440px',margin:'0 auto'}}>
+    <div  style={{width:'100%',maxWidth:'1440px',margin:'0 auto',minHeight:'100vh',display:'flex',flexDirection:'column',justifyContent:'space-between'}}>
         <Carousel/>
         <CardSection/>
         <CardSection/>
